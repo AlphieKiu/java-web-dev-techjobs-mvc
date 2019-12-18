cd89fe754087346d9473e3521cc18226730cf702
AlphieKiu <kiuhuaien87@gmail.com>
2019-12-17 20:46:52 -0600

    Done. might add bonuses later.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -26,19 +24,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping("/search")
+
+
+    @PostMapping("results")
     public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        ArrayList<Job> jobs;
+        jobs = JobData.findByColumnAndValue(searchType,searchTerm);
         model.addAttribute("searchType",searchType);
         model.addAttribute("searchTerm",searchTerm);
-        ArrayList<Job> jobs;
-        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
-            jobs = JobData.findAll();
-        } else {
-            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
-        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Search Condition: " + columnChoices.get(searchType) + " Search Term: " + searchTerm);
         model.addAttribute("jobs",jobs);
 
-        return "results";
+        return "search.html";
     }
 
 }
